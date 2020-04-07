0a04a9fd229c47f4cb9f6da3dff34e2b05e8cc1a
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-04-07 10:41:30 -0700

    Update VideoList, VideoEntry, and App Components

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,13 +1,13 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map(item => <VideoListEntry video={item} />)}
   </div>
 );
 
+
+//<div><h5><em></em> view goes here</h5></div>
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
@@ -17,3 +17,18 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+
+//ReactDOM.render(<VideoList videoList={exampleVideoData} />, document.getElementById('app')); // do this in the index.js file itself
+
+//include the Video List Entry component in each line of the video list
+//in each entry, include title, description, and thumbnail image
+
+
+
+// var GroceryListWithClass = (props) => (
+//   <ul>
+//     {props.groceryItems.map(item =>
+//       <GroceryListItemClass groceryItem={item} /> //new GroceryListItemClass ({myGroceryItems: myGroceryItems (array)})
+//     )}
+//   </ul>
+// );
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
 
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+//ReactDOM.render(<GroceryListWithClass groceryItems={myGroceryItems} />, listDiv);
+//document.getElementById("app")
+
